1f98e74593817c2e736429bdfaab296d53a84102
HenockAdane <adanehenock@gmail.com>
2021-11-11 20:27:36 +0000

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+// require('dotenv').config()
 const express = require("express");
 const port = process.env.PORT || 3001
 const app = express();
